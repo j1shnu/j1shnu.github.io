5b0f94f6e28bfd8001a43cbef7cc31a3785c4af9
j1shnu <jishnu.prasad4@gmail.com>
2021-08-28 23:07:14 +0530

    Updates

---
+++ precache-manifest.b768d727f52b81195538b13b77b86084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09abc8bb10c1d384f970a8464dc08e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b774405d983c9234ee",
+    "url": "/static/css/2.cb9e76b4.chunk.css"
+  },
+  {
+    "revision": "bfa9c6693986f42cd0ac",
+    "url": "/static/css/main.6fa1fca7.chunk.css"
+  },
+  {
+    "revision": "d6b774405d983c9234ee",
+    "url": "/static/js/2.eb95d5a4.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/static/js/2.eb95d5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa9c6693986f42cd0ac",
+    "url": "/static/js/main.3dc42c11.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
